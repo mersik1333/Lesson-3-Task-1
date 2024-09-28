2e5647ee8a691f3b9cb144eecbc1ee341f14764c
Даниил <@DESKTOP-FGHUH3E>
2024-09-28 09:02:58 +0300

    Добавьте файлы проекта.

---
+++ Task_1.py
@@ -0,0 +1,8 @@
+print("Введите вид питомца: ")
+Pet_type = input()
+print("Введите возраст питомца: ")
+Pet_age = input()
+print("Введите имя питомца: ")
+Pet_name = input()
+print("Это", Pet_type, "по кличке", "'", Pet_name,"'",".", "Возраст:", Pet_age)
+
